2d79f52ab67fbb915c03df8926f5bd3c43d7e824
pornarong <pornarong@gmail.com>
2019-10-18 11:33:27 +0700

    update template

--- src/crud/files/__name@dasherize__.service.ts
+++ src/crud/files/__name@dasherize__.service.ts
@@ -16,13 +16,37 @@ export class <%= classify(name) %>Service {
 
   /***** this is just mockup service *****/
   get<%= classify(name) %>List(param: ApiPageRequest<any>): Promise<ApiPageResponse<any>> {
-
+    return new Promise((resolve, reject) => {
+      const response: ApiPageResponse<any> = {
+        total: 10,
+        status: true,
+        data: [
+          { col1: 'Item 1 Col 1', col2: 'Item 1 Col 2', col3: 'Item 1 Col 3', col4: 'Item 1 Col 4'},
+          { col1: 'Item 2 Col 1', col2: 'Item 2 Col 2', col3: 'Item 2 Col 3', col4: 'Item 2 Col 4'},
+          { col1: 'Item 3 Col 1', col2: 'Item 3 Col 2', col3: 'Item 3 Col 3', col4: 'Item 3 Col 4'},
+          { col1: 'Item 4 Col 1', col2: 'Item 4 Col 2', col3: 'Item 4 Col 3', col4: 'Item 4 Col 4'},
+          { col1: 'Item 5 Col 1', col2: 'Item 5 Col 2', col3: 'Item 5 Col 3', col4: 'Item 5 Col 4'},
+        ]
+      };
+      resolve(response);
+    });
   }
   save<%= classify(name) %>(param: ApiRequest<any>): Promise<ApiResponse<any>> {
-    
+    return new Promise((resolve, reject) => {
+      const response: ApResponse<any> = {
+        status: true,
+        data: { col1: 'Item 1 Col 1', col2: 'Item 1 Col 2', col3: 'Item 1 Col 3', col4: 'Item 1 Col 4'}
+      };
+      resolve(response);
+    });
   }
   delete<%= classify(name) %>(param: ApiRequest<any>): Promise<ApiResponse<any>> {
-      
+    return new Promise((resolve, reject) => {
+      const response: ApResponse<any> = {
+        status: true
+      };
+      resolve(response);
+    });
   }
   /***** this is just mockup service *****/
 
